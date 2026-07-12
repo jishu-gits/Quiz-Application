fe5944e7c0a7f6be84caee7a9b173f50d898dd3a
Bidhan Manna <hero49045@gmail.com>
2026-07-12 20:15:23 +0530

    feat: add default Next.js configuration file

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
-  outputFileTracingRoot: __dirname,
+  /* config options here */
 };
 
 export default nextConfig;
